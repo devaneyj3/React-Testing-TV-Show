7a8c9be6c8300cfa56cc842ca6db730d160a72a5
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-04-22 22:48:23 -0400

    write test to pass api data into episode component

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './App';
-import { render } from '@testing-library/react';
+import { render, rerender } from '@testing-library/react';
 
 test('app renders', () => {
     render(<App/>)
--- src/episodes.test.js
+++ src/episodes.test.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { render, rerender } from '@testing-library/react';
+import Episodes from './components/Episodes';
 
-test('two plus two is 4', () => {
-    expect(2+2).toBe(4)
+test('renders episode data from api when episodes are passed through', () => {
+    const data = [{image: 'Chapter One', summary: 'Chapter One', runtime: 'Chapter One', number: 'Chapter One', name: 'Chapter One', season: 'Chapter One 1'}]
+    const { queryAllByText, rerender } = render(<Episodes episodes={[]}/>);
+
+    expect(queryAllByText(/chapter/i)).toHaveLength(0);
+
+    rerender(<Episodes episodes={data}/>)
+
+    expect(queryAllByText(/chapter/i)).toHaveLength(4);
+
 })
